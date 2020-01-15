18e9996621d1531a670cc3d5ccd4392dcca76be1
Ymin-dgnn <57888020+Ymin-dgnn@users.noreply.github.com>
2020-01-15 18:49:17 +0900

    first commit

---
+++ UI_presentation_1/UI_presentation_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI_presentation_1
+//
+//  Created by 임연지 on 2020/01/15.
+//  Copyright © 2020 임연지. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
